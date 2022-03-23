2eb0fded4527803e819e98eefc923c5ad8b39be2
minha721 <2minha721@gmail.com>
2022-03-23 17:50:25 +0900

    [CREATE] Create youtube player module

---
+++ youtube-player/src/main/java/com/example/youtube_player/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.youtube_player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
